9a72fb23e4b9348712a7bddf94fc0ef9566ba015
Guilherme Vieira <vieirasilvaguilherme70@gmail.com>
2023-09-09 16:57:56 -0300

    Classe CalculadoraJUNIT

--- src/CalculadoraJUNIT.java
+++ src/CalculadoraJUNIT.java
@@ -1,16 +1,22 @@
 public class CalculadoraJUNIT {
+    //Operacoes matematicas
+
+    //Metodo de soma
     public int soma(int a, int b){
         return a + b;
     }
 
+    //Metodo de subtracao
     public int subtracao(int a, int b){
         return a - b;
     }
 
+    //Metodo de divisao
     public int divisao(int a, int b){
         return a / b;
     }
 
+    //Metodo de multiplicacao
     public int multiplicacao(int a, int b){
         return a * b;
     }
